0ebc9755c620516777c13c7de41762c2608425b4
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2024-01-24 13:32:18 -0300

    RFF003: Componentes Input e Subtiltle1

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,9 @@ import img_login from "../assets/images/img_login.png";
 import TitleH3 from "@/components/TitleH3";
 import GoogleButton from "@/components/GoogleButton";
 import TitleH5 from "@/components/TitleH5";
+import Input from "@/components/Input";
 import LargeButton from "@/components/LargeButton";
+import Subtitle1 from "@/components/Subtitle1";
 import "../styles/login.css";
 
 function Login() {
@@ -12,8 +14,13 @@ function Login() {
       <div className="login-right-components">
         <TitleH3 text="Entre no Orange Portfólio" />
         <GoogleButton/>
-        <TitleH5 text="Faça login com email" />
-        <LargeButton text="ENTRAR" />
+        <div>
+          <TitleH5 text="Faça login com email" />
+          <Input label="Email address" type="email" name="email"/>
+          <Input label="Password" type="password" name="password"/>
+          <LargeButton text="ENTRAR" />
+          <Subtitle1 text="Cadastre-se" />
+        </div>
       </div>
       
     </div>
