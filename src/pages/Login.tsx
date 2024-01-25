8d870c8f33d22ac70eeab77ae7597cbbf564922d
Paulo Eduardo Cruz de Lemos Junior <pauloeduardo2906@gmail.com>
2024-01-25 02:15:28 -0300

    RFF005 - Alteração da tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,9 +9,17 @@ import "../styles/login-cadastro.css";
 
 function Login() {
   return (
+<<<<<<< Updated upstream
     <div className="login-cadastro">
       <img src={img_login.src}/>
       <div className="right-components">
+=======
+    <div className="login">
+      <div className="login-left-components">
+      <img src={img_login.src}/>
+      </div>
+      <div className="login-right-components">
+>>>>>>> Stashed changes
         <TitleH3 text="Entre no Orange Portfólio" />
         <GoogleButton/>
         <div>
