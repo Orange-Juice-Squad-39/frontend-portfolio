c4ec9ebfa116ebcc822d1f5afdb1a34cab82f1eb
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2024-01-24 23:57:15 -0300

    RFF004: botão de mostrar a senha na tela de Login

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,8 @@
+"use client"
+import { useState } from "react";
 import "../styles/input.css";
+import eye from "../assets/images/eye.svg";
+import invisibleEye from "../assets/images/invisibleEye.svg";
 
 interface inputProps {
     label: string;
@@ -7,12 +11,32 @@ interface inputProps {
 }
 
 function Input({ label, type, name }: inputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+  const inputType = showPassword ? "text" : type;
+
   return (
     <div className="input-container">
         <div className="label-container">
-            <label className="label-text">{label}</label>
+          <label className="label-text">{label}</label>
         </div>
-        <input type={type} name={name} className="input-content input-text"/>
+
+        <div className="input-content">
+          <input type={inputType} name={name} className="input-content input-text"/>
+
+          {type === "password" && (
+            <button
+              className="password-toggle"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword && (
+                <img src={eye.src} className="eye-button"/>
+              )}
+              {!showPassword && (
+                <img src={invisibleEye.src} className="eye-button"/>
+              )}
+            </button>
+          )}
+        </div>
     </div>
   )
 }
