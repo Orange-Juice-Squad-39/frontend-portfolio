9f5472887501da364807d4220a657c4678e75480
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2024-01-26 20:06:58 -0300

    RFF007: Componente visualizar publicação montado

--- src/components/ViewPost.tsx
+++ src/components/ViewPost.tsx
@@ -1,9 +1,16 @@
+"use client"
+import { useState } from "react";
 import close from "../assets/images/close.svg";
 import userImg from "../assets/images/perfil-image.png";
 import Tag from "./Tag";
 import "../styles/view-post.css";
 
 function ViewPost() {
+  const [avatar, setAvatar] = useState(userImg);
+  const [userName, setUserName] = useState('Camila Soares');
+  const [date, setDate] = useState('12/12');
+  const [downloadLink, setDownloadLink] = useState('https://gumroad.com/products/wxCSL');
+
   return (
     <div className="view-post-container">
       <button className="close-button">
@@ -11,16 +18,32 @@ function ViewPost() {
       </button>
       <div className="post-header">
         <div className="user-info">
-          <img src={userImg.src} className="user-img"/>
+          <img src={avatar.src} className="user-img"/>
           <div className="user-text">
-            <text className="user-name subtitle1">Camila Soares</text>
-            <text className="post-date subtitle1">12/12</text>
+            <text className="user-name subtitle1">{userName}</text>
+            <text className="post-date subtitle1">{date}</text>
           </div>
         </div>
         <h5 className="post-title">Ecommerce One Page</h5>
         <div className="post-tags">
           <Tag text="UX"/>
           <Tag text="Web"/>
+        </div>
+      </div>
+      <div className="post-img"></div>
+      <div className="post-description">
+        <text className="description-text body1">
+          Temos o prazer de compartilhar com vocês uma variação da nosso primeiro recurso gratuito, Monoceros. É um modelo de uma página para mostrar seus produtos. Tentamos redesenhar uma versão mais B2C e minimalista do nosso primeiro template de e-commerce.
+        </text>
+        <div className="download-container">
+          <text className="download-title subtitle1">Download</text>
+          <a 
+            href={downloadLink} 
+            target="_blank" 
+            className="download-link body2"
+          >
+            {downloadLink}
+          </a>
         </div>
       </div>
     </div>
