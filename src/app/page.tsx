312903b0bec75c6af44a75166672cce1adbf48a4
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2024-01-26 13:17:50 -0300

    RFF006: Montado o componente Add Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import styles from "./page.module.css";
 import Login from "../pages/Login";
 import Cadastro from "@/pages/Cadastro";
 import Portfolio from "@/pages/Portfolio";
+import AddProject from "@/components/AddProject";
 
 export default function Home() {
   return (
     <main>
       {/* <Login/> */}
       {/* <Cadastro/> */}
-      <Portfolio/>
+      {/* <Portfolio/> */}
+      <AddProject/>
     </main>
   );
 }
--- src/components/ButtonFirstProject.tsx
+++ src/components/ButtonFirstProject.tsx
@@ -4,13 +4,13 @@ import "../styles/button-first-project.css";
 function ButtonFirstProject() {
   return (
     <button className="first-project-container">
-        <img src={emptyImg.src} className="empty-img"/>
-        <div className="first-project-text">
-            <text className="body1">Adicione seu primeiro projeto</text>
-        </div>
-        <div className="first-project-text">
-            <text className="body2">Compartilhe seu talento com milhares de pessoas</text>
-        </div>
+      <img src={emptyImg.src} className="empty-img"/>
+      <div className="first-project-text">
+        <text className="body1">Adicione seu primeiro projeto</text>
+      </div>
+      <div className="first-project-text">
+        <text className="body2">Compartilhe seu talento com milhares de pessoas</text>
+      </div>
     </button>
   )
 }
