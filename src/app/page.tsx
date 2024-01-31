d51c1cbd541409460e1cd180e7645d7c02f572a6
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2024-01-31 03:46:57 -0300

    RFF013: Ajustando os inputs do componente de adicionar projetos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,22 @@ import Login from "./login/page";
 import Descobrir from "./descobrir/page";
 // import styles from "./page.module.css";
 
+import AddOrEditProject from "@/components/modals/add_or_edit_project";
+
 export default function Home() {
   const [logged, setLogged] = useState(false);
 
   return (
     <main>
-      {logged &&(
+      <AddOrEditProject adding={true}/>
+
+      {/* {logged &&(
         <Descobrir/>
       )}
 
       {!logged &&(
         <Login/>
-      )}
+      )} */}
     </main>
   );
 }
