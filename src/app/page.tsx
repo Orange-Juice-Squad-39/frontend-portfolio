0928757249d74f1759876242a9a3a4cde2bbef2e
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2024-02-01 15:23:14 -0300

    RFF013: Implementando o menu mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,18 @@ import Login from "./login/page";
 import Descobrir from "./descobrir/page";
 // import styles from "./page.module.css";
 
-import TagsInput from "@/components/tags_input";
-import AddOrEditProject from "@/components/modals/add_or_edit_project";
-
 export default function Home() {
   const [logged, setLogged] = useState(false);
 
   return (
     <main>
-      {/* {logged &&(
+      {logged &&(
         <Descobrir/>
       )}
 
       {!logged &&(
         <Login/>
-      )} */}
-
-      {/* <TagsInput label="" name="tags" placeholder="Tags"/> */}
-      <AddOrEditProject adding={true}/>
+      )}
     </main>
   );
 }
