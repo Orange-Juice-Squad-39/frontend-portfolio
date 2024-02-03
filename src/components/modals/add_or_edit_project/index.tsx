d0bd36dcac7a730f530be0b87ccc518cef65a3a7
Paulo Eduardo Cruz de Lemos Junior <pauloeduardo2906@gmail.com>
2024-02-03 20:57:13 -0300

    RFF019: Reparo no modal AddOrEditProject

--- src/components/modals/add_or_edit_project/index.tsx
+++ src/components/modals/add_or_edit_project/index.tsx
@@ -1,6 +1,4 @@
-// src\components\modals\add_or_edit_project.tsx
-
-"use client";
+"use client"
 import { useState } from "react";
 import emptyImg from "@/assets/images/collections.svg";
 import SmallButton from "@/components/small_button";
@@ -10,13 +8,14 @@ import "./style.css";
 
 interface addEditProps {
     adding?: boolean;
-    closeModal: () => void; // Adiciona a propriedade closeModal à interface
+    onCancel: () => void;
 }
 
-function AddOrEditProject({ adding, closeModal }: addEditProps) {
+function AddOrEditProject({ adding, onCancel }: addEditProps) {
     const [label, setLabel] = useState('');
     const [placeholder, setPlaceholder] = useState('Descrição');
     const [inputValue, setInputValue] = useState('');
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setInputValue(e.target.value);
@@ -36,13 +35,22 @@ function AddOrEditProject({ adding, closeModal }: addEditProps) {
         }
     };
 
-    const close = () => {
-        closeModal(); // Chama a função closeModal passada como propriedade
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files?.[0];
+        
+        if (selectedFile) {
+            // Lê o arquivo e converte para Data URL
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setSelectedImage(reader.result as string);
+            };
+            reader.readAsDataURL(selectedFile);
+        }
     };
 
     return (
-        <div className="add-project" onClick={close}>
-
+        <div className="add-project">
+            
             <div className="add-project-container">
 
                 <div className="add-project-header">
@@ -62,68 +70,107 @@ function AddOrEditProject({ adding, closeModal }: addEditProps) {
                         Selecione o conteúdo que você deseja fazer upload
                     </text>
 
-                    <div className="add-project-img">
-                        <button className="add-img-button">
+                    {/* <div className="add-project-img">
+                        <label className="add-img-button">
                             <div className="button-content">
-                                <img src={emptyImg.src} className="empty-img"/>
+                                <div className="img-container">
+                                    <img src={emptyImg.src} className="empty-img"/>
+                                </div>
                                 <div className="button-text">
                                     <text className="body2">
                                         Compartilhe seu talento com milhares de pessoas
                                     </text>
                                 </div>
                             </div>
-                        </button>
-                    </div>
+                            <input
+                                type="file"
+                                style={{ display: 'none' }}
+                                onChange={handleFileChange}
+                                // onClick={(e) => e.target.value = null} 
+                            />
+                        </label>
+                    </div> */}
+
+                <div className="add-project-img">
+                    <label className="add-img-button">
+                        <div className="button-content">
+                            <div className="img-container">
+                                {selectedImage ? (
+                                    <img src={selectedImage} className="selected-img" alt="Selected" />
+                                ) : (
+                                    <img src={emptyImg.src} className="empty-img" alt="Empty" />
+                                )}
+                            </div>
+                            {!selectedImage && (
+                                <div className="button-text">
+                                    <text className="body2">
+                                        Compartilhe seu talento com milhares de pessoas
+                                    </text>
+                                </div>
+                            )}
+                        </div>
+                        <input
+                            type="file"
+                            style={{ display: 'none' }}
+                            onChange={handleFileChange}
+                        />
+                    </label>
+                </div>
 
                     <a href="" className="view-post-button subtitle1">Visualizar publicação</a>
                     <div className="bottom-buttons">
                         <SmallButton text="SALVAR" disabled={false}/>
-                        <SmallButton text="CANCELAR" disabled={true}/>
+                        <SmallButton text="CANCELAR" disabled={false} onClick={onCancel}/>
                     </div>
 
                 </div>
+
 
                 <div className="add-project-right">
 
-                    <Input
-                        label=""
-                        type="text"
-                        name="title"
-                        placeholder="Título"
-                    />
+                <Input
+                    label=""
+                    type="text"
+                    name="title"
+                    placeholder="Título"
+                />
 
-                    <TagsInput
-                        label="" 
-                        name="tags" 
-                        placeholder="Tags"
-                    />
+                <TagsInput
+                    label="" 
+                    name="tags" 
+                    placeholder="Tags"
+                />
 
-                    <Input
-                        label=""
-                        type="url"
-                        name="link"
-                        placeholder="Link"
+                <Input
+                    label=""
+                    type="url"
+                    name="link"
+                    placeholder="Link"
+                />
+                        
+                <div className="description-container">
+                    <textarea 
+                        name="description" 
+                        placeholder={placeholder}
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                        className="description-input"
                     />
-                    
-                    <div className="description-container">
-                        <textarea 
-                            name="description" 
-                            placeholder={placeholder}
-                            value={inputValue}
-                            onChange={handleInputChange}
-                            onFocus={handleFocus}
-                            onBlur={handleBlur}
-                            className="description-input"
-                        />
-                        <label className="description-label">{label}</label>
-                    </div>
+                    <label className="description-label">{label}</label>
+                </div>
 
                 </div>
 
+
+
+
             </div>
 
+
         </div>
-    );
+    )
 }
 
-export default AddOrEditProject;
+export default AddOrEditProject
